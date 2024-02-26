326d2bc0be2778c93fd5b02b37b1b40b3cdd54b5
AlexCodesApps <141887614+AlexCodesApps@users.noreply.github.com>
2024-02-26 08:19:38 -0500

    Add files via upload
    (WIP) Dynamic Arrays library in C. This Library is a beginner project to support the dynamic allocation of arrays in C, while abstracting away some of the annoyances. The functions use ptrs to achieve "generic" datatype function, which can be replicated for one of vars by using a function cast for the corresponding datatype. Would like to receive feedback. TODO - When finished compile into a properly downloadable Lib.

---
+++ dynarray.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#pragma once
+
+typedef struct DynamicArray{
+    void *ptr;
+    size_t element_size;
+    int length;
+} DynamicArray;
+
+void CreateDynamicArray(DynamicArray *array, size_t element, int numOfSlots);
+void AddToArray(DynamicArray *array, void *data);
+void *ReadIndex(DynamicArray *array, int index);
+void RemoveElement(DynamicArray *array, void *element);
